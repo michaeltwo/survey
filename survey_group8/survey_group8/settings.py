60d51d2a75e1d5f6917b55b1a6aa23231de91431
MIC <weihua.zheng@hotmail.com>
2024-10-29 23:02:45 -0400

    Update settings.py
    adding login logout redirect urls in settings

--- survey_group8/survey_group8/settings.py
+++ survey_group8/survey_group8/settings.py
@@ -121,3 +121,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#login logout redirecting
+LOGIN_REDIRECT_URL = '/profile/'
+LOGOUT_REDIRECT_URL = '/login/'
