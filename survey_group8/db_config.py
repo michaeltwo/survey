14bab2e6c9eaf04888eb39bff1b1de8f6114556c
michael_weihua_zheng <weihua.zheng@hotmail.com>
2024-10-31 16:10:56 -0400

    main

--- survey_group8/db_config.py
+++ survey_group8/db_config.py
@@ -3,7 +3,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'survey_group8',
         'USER': 'postgres',
-        'PASSWORD': 'password', # Your Postgres Password
+        'PASSWORD': 'admin', # Your Postgres Password
         'HOST': 'localhost',  
         'PORT': '5432',       
     }
