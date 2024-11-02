f7c67917fe284904e7b04db6f7d3e11847fb7bab
fasmim0 <fasmim0@gmail.com>
2024-11-02 12:03:11 -0400

    used published from survey to generate html view o

--- survey_group8/results/views.py
+++ survey_group8/results/views.py
@@ -5,40 +5,46 @@ from django.db.models import Max
 
 def survey_results_published(request, id):
     survey = Surveys.objects.get(id=id)
-    questions = survey.questions.all()  # Get all questions in the survey
+    questions = survey.questions.all()  
 
-    # Get the highest republished version for the survey
-    latest_version = Results.objects.filter(survey_id=survey).aggregate(max_version=Max('republished_version'))['max_version']
-
-    # Calculate the total number of unique respondents for the latest version
-    total_respondents = Results.objects.filter(survey_id=survey, republished_version=latest_version).values('user_id').distinct().count()
+    max_republished = Surveys.objects.filter(pk=id).aggregate(Max('republished'))['republished__max'] #
+   
+    total_respondents = Results.objects.filter(survey_id=survey, republished_version=max_republished).values('user_id').distinct().count()
 
     question_data = []
-    for question in questions:
-        answer_data = []
-        
-        # Count total responses for this question in the latest version
-        total_responses = Results.objects.filter(question_id=question, republished_version=latest_version).count()
+    total_respondents = Results.objects.filter(survey_id=survey, republished_version=max_republished).count()
 
-        for answer in question.answers.all():
-            # Get the count of responses for this answer in the latest version
-            count = Results.objects.filter(question_id=question, answer_id=answer, republished_version=latest_version).count()
-            percentage = (count / total_responses * 100) if total_responses > 0 else 0
-            answer_data.append({
-                'answer_text': answer.answer,
-                'count': count,
-                'percentage': percentage,
+    if total_respondents > 0:
+        results = Results.objects.filter(survey_id=survey, republished_version=max_republished)
+
+        for question in questions:
+            answers = Answers.objects.filter(question_id=question)
+
+            answer_data = []
+            for answer in answers:
+                response_count = results.filter(answer_id=answer).count()
+                percentage = (response_count / total_respondents * 100) if total_respondents > 0 else 0
+
+                answer_data.append({
+                    'answer_text': answer.answer,
+                    'count': response_count,
+                    'percentage': percentage,
+                })
+
+            question_data.append({
+                'question_text': question.question,
+                'answers': answer_data,
             })
-
+    else:
         question_data.append({
-            'question_text': question.question,
-            'answers': answer_data,
+            'question_text': "No responses yet",
+            'answers': []
         })
 
     context = {
         'survey': survey,
         'question_data': question_data,
         'total_respondents': total_respondents,
-        'latest_version': latest_version,
     }
+
     return render(request, 'results.html', context)
