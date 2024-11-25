adf59745af7840609b98cd7134ffee3191f263e6
Dev Sanjay Shah <shah.dev19@gmail.com>
2024-11-25 16:01:58 -0500

    changes

--- survey_group8/accounts/signals.py
+++ survey_group8/accounts/signals.py
@@ -5,6 +5,6 @@ from django.dispatch import receiver
 
 @receiver(post_migrate)
 def create_groups(sender, **kwargs):
-    if sender.name == 'accounts':  # Ensure it runs only for the 'accounts' app
+    if sender.name == 'accounts':  # Ensure it runs only for  'accounts' in the app
         Group.objects.get_or_create(name='Taker')
         Group.objects.get_or_create(name='Creator')
