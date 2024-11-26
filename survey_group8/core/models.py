38908996de7caf1ba5ce5a02282d413f923e1c44
Shubham Junnarkar <shubhamjunnarkar75@gmail.com>
2024-11-26 23:21:05 +0530

    Changes by shubham

--- survey_group8/core/models.py
+++ survey_group8/core/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# For user and roles, using built-in auth_user table, User
+# For users and roles, utilizing the built-in auth_user table, User
 class Surveys(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
