ee604f7a54ac99426ca607e75ff505b093a43e32
michael_weihua_zheng <weihua.zheng@hotmail.com>
2024-11-27 18:42:40 -0500

    adjust static

--- survey_group8/survey_group8/settings.py
+++ survey_group8/survey_group8/settings.py
@@ -123,9 +123,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # This includes your_project/static
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),  # This includes your_project/static
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
