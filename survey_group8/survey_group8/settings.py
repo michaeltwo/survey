0584d5dfe1c6d2f783c6ac2de0ac1c753cf498b4
michael_weihua_zheng <weihua.zheng@hotmail.com>
2024-11-27 18:05:43 -0500

    add static root in settings

--- survey_group8/survey_group8/settings.py
+++ survey_group8/survey_group8/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 
