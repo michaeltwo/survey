7252be0fe593a7d9574abf332a22a51e679d92eb
michael_weihua_zheng <weihua.zheng@hotmail.com>
2024-11-28 01:03:28 -0500

    add entrypoint

---
+++ survey_group8/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Wait for PostgreSQL to be ready
+./scripts/wait-for-it.sh db:5432 -- python manage.py makemigrations && python manage.py migrate
+
+# Start Gunicorn
+exec gunicorn --bind 0.0.0.0:8000 survey_group8.wsgi:application
