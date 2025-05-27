3f87f740bfb12163f954ab56f93188bfd6d5a0ae
MIC <weihua.zheng@hotmail.com>
2025-05-27 17:08:51 -0400

    Update settings.py

--- survey_group8/survey_group8/settings.py
+++ survey_group8/survey_group8/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_URL = '/static/'
+STATIC_URL = '/survey/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),  # This includes your_project/static
